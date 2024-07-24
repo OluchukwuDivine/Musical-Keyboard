b725cbd06666506d4f3444108e4fd927a274381a
Oluchukwu Divine <johndoe@example.com>
2024-07-24 11:47:14 +0100

    updated

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const dohAudio = new Audio("sounds/doh.wav");
+const rehAudio = new Audio("sounds/reh.wav");
+const mihAudio = new Audio("sounds/mih.wav");
+const fahAudio = new Audio("sounds/fah.wav");
+const sohAudio = new Audio("sounds/soh.wav");
+const lahAudio = new Audio("sounds/lah.wav");
+const tihAudio = new Audio("sounds/tih.wav");
+const bigDohAudio = new Audio("sounds/big-doh.wav");
+const octaveOneAudio = new Audio("sounds/octave.wav");
+const octaveTwoAudio = new Audio("sounds/octave2.wav");
+const octaveThreeAudio = new Audio("sounds/octave3.wav");
+
 const dohBtn = document.querySelector(".doh-btn");
 const rehBtn = document.querySelector(".reh-btn");
 const mihBtn = document.querySelector(".mih-btn");
@@ -10,19 +22,6 @@ const octaveOneBtn = document.querySelector(".octave-btn-1");
 const octaveTwoBtn = document.querySelector(".octave-btn-2");
 const octaveThreeBtn = document.querySelector(".octave-btn-3");
 const displayNote = document.querySelector(".display-note");
-
-
-const dohAudio = new Audio("sounds/doh.wav");
-const rehAudio = new Audio("sounds/reh.wav");
-const mihAudio = new Audio("sounds/mih.wav");
-const fahAudio = new Audio("sounds/fah.wav");
-const sohAudio = new Audio("sounds/soh.wav");
-const lahAudio = new Audio("sounds/lah.wav");
-const tihAudio = new Audio("sounds/tih.wav");
-const bigDohAudio = new Audio("sounds/big-doh.wav");
-const octaveOneAudio = new Audio("sounds/octave.wav");
-const octaveTwoAudio = new Audio("sounds/octave2.wav");
-const octaveThreeAudio = new Audio("sounds/octave3.wav");
 
 
 dohBtn.addEventListener("click", () => {
